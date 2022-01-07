b86a876bf606ab888190b98c2646692c2734dc38
MarcoOliveira2 <fermandesmarco@gmail.com>
2022-01-07 23:17:56 +0000

    Requests

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private http: HttpClient, private activatedRoute: ActivatedRoute) { }
+  baseUrl: string = `http://localhost:3001/`;
+  jwtToken: string = "";
   ngOnInit(): void {
+
+  }
+
+  onLogin(email: string, password: string) {
+    // return this.http.post(this.baseUrl + `/auth/signin`, { email, password })
+    // {
+
+    // }
   }
 
 }
